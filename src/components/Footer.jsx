96fdf0d699f136b30e4b0d99231d7b483cd17bb3
Ramit <ramitsarkar902@gmail.com>
2021-10-21 22:15:44 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
+      © 2021 Made with ❤ from Goomgle
+      <h3>Ramit Sarkar</h3>
+    </div>
+  );
+}
+
+export default Footer;
